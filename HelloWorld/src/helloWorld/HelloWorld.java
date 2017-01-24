0bfe85c40c83e172fd0db61fb9b94a03614ecfa4
Daniel <Daniel@10.102.89.208>
2017-01-23 22:16:55 -0800

    For the assignment due on Wednesday.

---
+++ HelloWorld/src/helloWorld/HelloWorld.java
@@ -0,0 +1,23 @@
+/* Assignment 1 - Hello World by Daniel Castillo
+ * Written in Java
+ * Assigned: Monday, January 23, 2017
+ * Due: Wednesday, January 25, 2017
+ */
+
+
+
+
+package helloWorld;
+
+public class HelloWorld 
+{ // begin class HelloWorld
+
+	public static void main(String[] args) 
+	{ // begin main
+		
+		String message = "Hello World";
+		System.out.println(message);
+
+	} // end main
+
+} // end class HelloWorld
