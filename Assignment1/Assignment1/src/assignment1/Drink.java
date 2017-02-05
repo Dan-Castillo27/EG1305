ff2f6535f325b6252c39a48b43e749678030b0a9
aktunc-STMU <aktunc@gmail.com>
2017-02-04 22:58:26 -0600

    Check my comments

--- Assignment1/Assignment1/src/assignment1/Drink.java
+++ Assignment1/Assignment1/src/assignment1/Drink.java
@@ -7,7 +7,8 @@ public class Drink
 	{ // begin main
 		int customers = 12467;
 		double perWeek = (.14*customers);
-		double citrusPeople = (.64*customers);
+		double citrusPeople = (.64*customers); /*this is not correct as 64 percent of energy drink users prefer citrus not 
+		64 percent of the customers, should be .14 * .64 * customers. Also include spaces on both sides of operators*/
 		
 		System.out.println("14% of customers, or approximately "+(int)perWeek+" people, buy one or more energy drinks per week.");
 		System.out.println("With 64% of the customers, or approximately "+(int)citrusPeople+" people, prefer the citrus flavored drink");
