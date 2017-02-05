0f0f56014bbf9206b56544d26c79bd83d61c8457
aktunc-STMU <aktunc@gmail.com>
2017-02-04 23:05:35 -0600

    Incorrect formula, check comments.

--- Assignment1/Assignment1/src/assignment1/StockTransaction.java
+++ Assignment1/Assignment1/src/assignment1/StockTransaction.java
@@ -16,6 +16,9 @@ public class StockTransaction
 		double firstCom = (COMMISSION_RATE)*(numShares*buyPerShare);
 		double secCom = (COMMISSION_RATE)*(numShares*sellPerShare);
 		double profit = ((numShares*sellPerShare)-secCom) - ((numShares*buyPerShare)-firstCom);
+		/* This formula is not correct. First calculate the profit: sold - bought, then take off the commission to find the net profit.
+		 should be double profit = ((numShares*sellPerShare - numShares*buyPerShare)) - (secCom+firstCom);
+		 */
 		
 		System.out.print("Joe paid $"+(numShares * buyPerShare)+" for 1000 shares of stock, $"+buyPerShare+" per share. He paid his stockbroker a 2% commission,");
 		System.out.println(" totalling to $"+formatter.format(firstCom));
